02e22553fc79e11fd65ddbca8afffdff6a53a891
rahulkumarrmk <rahul03266@gmail.com>
2020-10-03 17:38:59 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./style.css";
 const student = ["one","two","three","four"];
 
 
 class App{
+
+
+
+ onTest(){
+
+
+ReactDOM.render(<p>Hello</p>, document.getElementById('test'));
+ }
   
+
+  reneder(){
+    return(
+
+      <div>
+     <div id='test'>
+     </div>
+     <h1> click</h1>
+      </div>
+    );
+  };
 }
+
+export default App;
